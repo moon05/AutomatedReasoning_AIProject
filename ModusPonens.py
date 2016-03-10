67c979cea631a797d7a32c528b237dfa6fe434d4
Abdullah Al Mamun <amamun@u.rochester.edu>
2016-03-09 23:13:09 -0500

    truthtable generator code added

--- ModusPonens.py
+++ ModusPonens.py
@@ -30,20 +30,36 @@ def enum(**enums):
 	return type('Enum', (), enums)
 
 
-# def modelCheck(dictionary):
-
-# 	if (type(dictionary)!=dict):
-# 		break
-# 	if (dictionary.)
-
 def ListReverse(List):
 	
 	temp = List
 	temp = temp[::-1]
 	return temp
 
+# A list with the variables will be the input
+# The TruthTable for n variables will be generated
+# The table's values will be matched with respective variables
+# A list with 2^n dictionarties will be returned
+
+def TableGenerator(ListVar):
+
+	List_of_Variables = ListVar
+	length_vars = len(List_of_Variables)
+	values = [True, False]
+	truth_table = list(itertools.product(values,repeat=length_vars))
+	table_list = list()
+	
+	for n in truth_table:
+		d = dict()
+		for i in range(length_vars):
+			d[vart[i]] = n[i]
+		table_list.append(d)
+	
+	return table_list
 
 #assigning values to those sentences similar to "A" or "not A"
+#I will make it work for compound sentences soon
+#but for probem 1 it doesn't need to work for compound sentences
 def TruthFinder(dictionary):
 
 	if (type(dictionary) != dict):
@@ -69,17 +85,17 @@ def TruthFinder(dictionary):
 
 
 #this will be for IMPLIES and IFF
-def ModelChecker(dictionary):
+# def ModelChecker(dictionary):
 
-	if (type(dictionary) != dict):
-		print "This is not a dictionary"
-		print "Please enter correct type of variable"
-		return
+# 	if (type(dictionary) != dict):
+# 		print "This is not a dictionary"
+# 		print "Please enter correct type of variable"
+# 		return
 	
-	temp = dictionary.keys()
-	length = len(temp)
+# 	temp = dictionary.keys()
+# 	length = len(temp)
 	
-	if (length > 2):
+# 	if (length > 2):
 		
 #Connectives
 BinaryConnective = enum(AND = 1, OR = 2, IMPLIES = 3, IFF = 4)
