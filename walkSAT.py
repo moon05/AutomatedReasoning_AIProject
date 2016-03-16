14be0dea4008debd47ef8bdd0bae21dbf12e7867
Abdullah Al Mamun <amamun@u.rochester.edu>
2016-03-16 11:28:18 -0400

    walkSAT still in progress

---
+++ walkSAT.py
@@ -0,0 +1,86 @@
+import truthtable
+import random
+
+presentvariables = []
+len_Vars = len(presentvariables)
+
+def flip(truthRow,Var):
+	
+	Row = truthRow
+	if (Row[Var] == True):
+		Row[Var] = False
+	elif (Row[Var] == False):
+		Row[Var] = True
+
+	return Row
+
+def satisfies(clause, Row):
+
+	s = None
+	
+	if (len(clause)==3):
+		s = Row[clause[0]] or Row[clause[2]]
+	elif (len(clause)==4 and clause[0] == "not"):
+		s = not Row[clause[1]] or Row[clause[3]]
+	elif (len(clause)==4 and clause[2] == "not"):
+		s = Row[clause[0]] or not Row[clause[3]]
+	elif (len(clause)==5 and clause[0] == "not" and clause[3]=="not"):
+		s = not Row[clause[0]] or not Row[clause[4]]
+
+	return s
+
+def numSatisfied(clauseList, Row):
+
+	number = 0
+
+	for n in clauseList:
+		k = satisfies(n,Row)
+		if (k is True):
+			number = number + 1
+
+	return number
+
+def varMaxesSAT(clauseList, VARS, Row):
+
+
+
+def WalkSAT(clauses, probability, maxflips):
+
+	for i in range(maxflips):
+		try:
+		tempTable = truthtable.TableGenerator[presentvariables]
+		len_of_table = tempTable
+
+		
+		model = tempTable[0]
+		for i in range(0):
+			
+			s = list()
+			for n in clauses:
+
+				sat = satisfies(n, model)
+				s.append()
+
+			if (False not in s):
+				return model
+
+			if (random.uniform(0,1) >= probability):
+				temp_model = model
+				index_var_to_change = random.randint(0,len_Vars-1)
+				temp_list_model = list(model)
+				var_to_change = temp_list_model[index_var_to_change]
+				model = flip(temp_model, var_to_change)
+
+			else:
+				
+
+		return "Failure"
+
+		except IndexError:
+			return "Failure"
+
+
+
+
+
+
