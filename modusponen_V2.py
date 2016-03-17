789d19029e0820e9f9a4e0339397a81bb5c51c2e
Abdullah Al Mamun <amamun@u.rochester.edu>
2016-03-17 00:18:17 -0400

    last update with CNF

--- modusponen_V2.py
+++ modusponen_V2.py
@@ -1,5 +1,6 @@
 import truthtable
 import collections
+import CNF
 
 presentVariables = ["P", "Q"]
 
@@ -9,20 +10,32 @@ Table = truthtable.TableGenerator(presentVariables)
 #sentence 1
 def R1(coord):
 	for n in Table:
-		n["R1"] = n[coord]
+		n["=>R1"] = n[coord]
 
 def R2():
 	for n in Table:
-		n["R2"] = not n["P"] or n["Q"]
+		n["=>R2"] = CNF.valueConverter(n, ["NOT","OR","P","Q"], presentVariables)
+def KB():
+	for n in Table:
+		if ( n["=>R1"] and n["=>R2"] is True ):
+			n["KB"] = True
+		else:
+			n["KB"] = False
+
+
 
 def entailment():
 	for n in Table:
-		if (n["R1"] and n["R2"] is True):
-			if (n["P"] and n["Q"] is True):
-				print "Entails"
-		return
+		if (n["KB"] is True):
+			if (n["Q"] is False):
+				print "Doesn't Entail"
+				return
+	
+	print "Entails"
+	return
 
 
 R1("P")
 R2()
+KB()
 entailment()
